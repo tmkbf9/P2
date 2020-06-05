3e60d1d87d5139321cc7a3f08734a84a14bd8082
tmkbf9 <tmkbf9@mail.umsl.edu>
2020-06-05 13:01:43 -0500

    worked on P2

--- P2.cpp
+++ P2.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
-
+#include <fstream>
 #include "scanner.h"
 #include "parser.h"
 #include "test_tree.h"
 #include "node.h"
+#include "token.h"
 
 using namespace std;
 
-int main(int argc, char ** arvgv) {
-  Scanner scanner(cin, cerr);
-  Parser parser(scanner);
-  TestTree printer;
-  
-  Node * rootNode = parser.parse();
-  printer.print(rootNode, cout);
+int main(int argc, char** argv) {
+    TestTree printer;
+    token errorToken;
+    istream& is;
+
+    if (argc == 1) { // read from stdin
+        Scanner scanner(cin, cerr);
+        Parser parser(scanner, errorToken);
+        Node* rootNode = parser.parse();
+        printer.printTree(rootNode, cout);
+    }
+    else if (argc == 2) {// read from input file
+        ifstream ifs(argv[1]);
+        Scanner scanner(ifs, cerr);
+        Parser parser(scanner, errorToken);
+        Node* rootNode = parser.parse();
+        printer.printTree(rootNode, cout);
+  }
+  else {
+      cerr << "Two many command line parameters given. Aborting" << endl;
+      return 1;
+  }
 
   return(0);
 }
